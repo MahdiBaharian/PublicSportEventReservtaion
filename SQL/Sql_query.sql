9902a398f7e3fb38e98535ee9cd6532ec92e47f1
MahdiBaharian <mahdibaharian1384@gmail.com>
2026-08-03 21:18:00 +0330

    feat(db): add analytical queries 11 to 15

--- SQL/Sql_query.sql
+++ SQL/Sql_query.sql
@@ -85,3 +85,45 @@ WHERE r.reservation_status = 'paid'
       ORDER BY created_at ASC
       LIMIT 1
   );
+
+-- Query 11
+SELECT first_name, last_name
+FROM users
+WHERE role = 'admin';
+
+-- Query 12
+SELECT u.first_name, u.last_name
+FROM users u
+JOIN reservations r ON u.user_id = r.user_id
+WHERE r.reservation_status = 'paid'
+GROUP BY u.user_id, u.first_name, u.last_name
+HAVING SUM(r.quantity) >= 2;
+
+-- Query 13
+SELECT u.first_name, u.last_name
+FROM users u
+JOIN reservations r ON u.user_id = r.user_id
+JOIN tickets t ON r.ticket_id = t.ticket_id
+WHERE r.reservation_status = 'paid' 
+  AND t.sport_type = 'football'
+GROUP BY u.user_id, u.first_name, u.last_name
+HAVING SUM(r.quantity) <= 2;
+
+-- Query 14
+SELECT u.email, u.phone_number
+FROM users u
+JOIN reservations r ON u.user_id = r.user_id
+JOIN tickets t ON r.ticket_id = t.ticket_id
+WHERE r.reservation_status = 'paid'
+  AND t.sport_type IN ('football', 'volleyball', 'basketball')
+GROUP BY u.user_id, u.email, u.phone_number
+HAVING COUNT(DISTINCT t.sport_type) = 3;
+
+-- Query 15
+SELECT t.*, p.paid_at
+FROM tickets t
+JOIN reservations r ON t.ticket_id = r.ticket_id
+JOIN payments p ON r.reservation_id = p.reservation_id
+WHERE p.transaction_status = 'success'
+  AND DATE(p.paid_at) = CURRENT_DATE
+ORDER BY p.paid_at ASC;
